4edd56d137e84ad8dbaa12ea745b8adab84877e3
nan <ipfilus@gmail.com>
2024-10-14 22:15:06 -0300

    feat: add login/logout routes

--- app.py
+++ app.py
@@ -1,12 +1,47 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from database import db
 from models.user import User
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 
 app = Flask(__name__)
+
 app.config['SECRET_KEY'] = 'super-secret-key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+login_manager = LoginManager(app)
+
 db.init_app(app)
 
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+
+    if username and password:
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.password == password:
+            login_user(user)
+            print(current_user.is_authenticated)
+            return jsonify({'message': 'Login successful'}), 200
+        else:
+            return jsonify({'message': 'Invalid credentials'}), 401
+    else:
+        return jsonify({'message': 'Username and password are required'}), 400
+
+@app.route('/logout', methods=['GET'])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({'message': 'Logout successful'}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
